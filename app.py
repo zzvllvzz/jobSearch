65e5b450fdcec8619c33e70f550fed8cdb720a52
null <229582687+zzvllvzz@users.noreply.github.com>
2025-11-14 23:27:54 +0200

    feat(req): freeze requirements.txt

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('pageNotFound.html'), 404
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
